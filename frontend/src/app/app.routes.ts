da807a0e60c0324b4bedd6a3788c0eaeb0407259
jihen <jihen.hajdahmen@gmail.com>
2025-11-20 16:29:52 +0100

    test

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,31 +1,18 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
-import { Connexion } from './connexion/connexion';
-import { Inscription } from './inscription/inscription';
 import { Accueil } from './accueil/accueil';
 import { Magasin } from './magasin/magasin';
 import { Categorie } from './categorie/categorie';
 import { Produit } from './produit/produit';
-import { Ajout } from './ajout/ajout';
-import { Ajoutcat } from './ajoutcat/ajoutcat';
-import { Ajoutprod  } from './ajoutprod/ajoutprod';
-import { Contact } from './contact/contact'; // ← Importez le composant Contact
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
-    { 
-    path: 'inscription', 
-    component: Inscription
-  },
-  { 
-    path: 'connexion', 
-    component: Connexion
-  },
   { 
     path: 'accueil', 
     component: Accueil
   },
   { 
-    path: 'contact',  // ← Ajoutez cette route
+    path: 'contact',
     component: Contact
   },
   { 
@@ -39,18 +26,6 @@ export const routes: Routes = [
   { 
     path: 'produit', 
     component: Produit
-  },
-  { 
-    path: 'ajout', 
-    component: Ajout
-  },
-  { 
-    path: 'ajoutcat', 
-    component: Ajoutcat
-  },
-  { 
-    path: 'ajoutprod', 
-    component: Ajoutprod
   },
   { 
     path: '',  // ← Route par défaut
